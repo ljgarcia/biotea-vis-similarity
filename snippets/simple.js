caacbaf6d8b432b8faa7f88a3c4e99e6942da727
ljgarcia <ljgarcia@users.noreply.github.com>
2016-01-20 18:43:58 +0000

    Update simple.js

--- snippets/simple.js
+++ snippets/simple.js
@@ -3,9 +3,10 @@ var app = require("biotea-vis-similarity");
 var instance = new app({
     el: yourDiv,
     path: 'http://localhost:9090/snippets/data/',
-    queryId: 55328,
+    queryId: 117238,
     db: 'PMC',
-    relatedIds: [59472]
+    relatedIds: [55328, 59472, 64840, 115224, 115254, 126238, 126242, 126873, 130049, 133446, 151173, 153502, 165429,
+        166151, 194174, 200968, 212256, 222958]
 });
 
 instance.getDispatcher().on('ready', function(d) {
