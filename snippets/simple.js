510434ceefdd18820aefd0a722c51204409a60e7
ljgarcia <leylajael@gmail.com>
2016-04-30 21:45:13 +0100

    Use tooltip viewer and add support for data

--- snippets/simple.js
+++ snippets/simple.js
@@ -4,7 +4,7 @@ var instance = new app({
     el: yourDiv,
     path: 'http://localhost:9090/snippets/data/',
     queryId: 117238,
-    db: 'PMC',
+    prefixId: 'PMC',
     relatedIds: [55328, 59472, 64840, 115224, 115254, 126238, 126242, 126873, 130049, 133446, 151173, 153502, 165429,
         166151, 194174, 200968, 212256, 222958]
 });
